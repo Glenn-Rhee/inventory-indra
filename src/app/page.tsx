32229d67c70896533bd3610fcbadfe3c3d2c18ab
Glenn-Rhee <glennviktor5@gmail.com>
2026-05-19 11:50:14 +0700

    feat: added error handling at server component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { Metadata } from "next";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { DataStatsResponse, ResponsePayload } from "@/types";
+import ResponseError from "@/error/ResponseError";
 
 export const metadata: Metadata = {
   title: "Dashboard",
@@ -16,40 +17,43 @@ const BASEURL = process.env.NEXT_PUBLIC_BASE_SERVER_URL;
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
-  const res = await fetch(BASEURL + "/stats?rangeType=90", {
-    method: "GET",
-    headers: {
-      "x-user-id": session?.user.id || "",
-    },
-  });
-
-  const dataRes = (await res.json()) as ResponsePayload<DataStatsResponse>;
-  const data = dataRes.data;
+  let errorMsg: string | null = null;
+  let data: DataStatsResponse = {
+    BestSeller: "",
+    Stocks: 0,
+    TotalRevenue: 0,
+    TotalTransactions: 0,
+    DataChart: [],
+  };
+  try {
+    const res = await fetch(BASEURL + "/stats?rangeType=90", {
+      method: "GET",
+      headers: {
+        "x-user-id": session?.user.id || "",
+      },
+    });
+    const dataRes = (await res.json()) as ResponsePayload<DataStatsResponse>;
+    if (dataRes.status === "failed") {
+      throw new ResponseError(res.status, dataRes.message);
+    }
+    data = dataRes.data;
+  } catch (error) {
+    if (error instanceof ResponseError) {
+      errorMsg = error.message;
+    } else {
+      errorMsg = "An error occured! Please try again later.";
+    }
+  }
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
         <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          {dataRes.status === "failed" ? (
-            <SectionCards
-              data={{
-                BestSeller: "",
-                Stocks: 0,
-                TotalRevenue: 0,
-                TotalTransactions: 0,
-              }}
-            />
-          ) : (
-            <SectionCards data={data} />
-          )}
+          <SectionCards errorMsg={errorMsg} data={data} />
           <div className="px-4 lg:px-6">
-            {dataRes.status === "failed" ? (
-              <ChartAreaInteractive userId={session?.user.id || ""} data={[]} />
-            ) : (
-              <ChartAreaInteractive
-                userId={session?.user.id || ""}
-                data={data.DataChart}
-              />
-            )}
+            <ChartAreaInteractive
+              userId={session?.user.id || ""}
+              data={data.DataChart}
+            />
           </div>
           <DataTable userId={session?.user.id || ""} />
         </div>
--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -12,13 +12,18 @@ import {
 import { formatRupiah } from "@/helper/getFormatRupiah";
 import { DataStatsResponse } from "@/types";
 import { TrendingUpIcon, TrendingDownIcon } from "lucide-react";
+import { toast } from "sonner";
 
 interface SectionCardsProps {
   data: Omit<DataStatsResponse, "DataChart">;
+  errorMsg?: string | null;
 }
 
 export function SectionCards(props: SectionCardsProps) {
-  const { data } = props;
+  const { data, errorMsg } = props;
+  if (errorMsg) {
+    toast.error(errorMsg);
+  }
   return (
     <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
       <Card className="@container/card">
