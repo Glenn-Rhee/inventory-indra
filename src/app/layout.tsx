ad47b2d478527318767ceabbcc54ec6331d389e0
Glenn-Rhee <glennviktor5@gmail.com>
2026-05-26 21:34:00 +0700

    feat: added robots

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,6 +50,26 @@ export const metadata: Metadata = {
       "max-snippet": -1,
     },
   },
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/icons/icon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icons/icon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [
+      {
+        url: "/icons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/icons/safari-pinned-tab.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
