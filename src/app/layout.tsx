3b1a17c75d001fb6e6cd1e71793a3e76d95b6d35
Glenn-Rhee <glennviktor5@gmail.com>
2026-05-29 21:08:03 +0700

    feat: added icon at title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,13 +60,13 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico" },
-      { url: "/icons/icon-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/icons/icon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/logo-icon.png" },
+      { url: "/logo-icon.png", sizes: "16x16", type: "image/png" },
+      { url: "/logo-icon.png", sizes: "32x32", type: "image/png" },
     ],
     apple: [
       {
-        url: "/icons/apple-touch-icon.png",
+        url: "/logo-icon.png",
         sizes: "180x180",
         type: "image/png",
       },
@@ -74,7 +74,7 @@ export const metadata: Metadata = {
     other: [
       {
         rel: "mask-icon",
-        url: "/icons/safari-pinned-tab.svg",
+        url: "/logo-icon.png",
       },
     ],
   },
